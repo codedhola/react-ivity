931a412ee65c6caa5c025cd01db4ea029714a1be
codedhola <codedhola2000@gmail.com>
2023-08-07 23:40:57 +0100

    updated features

--- src/AddFriend.jsx
+++ src/AddFriend.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddFriend = () => {
+const AddFriend = ({ onAddFriend }) => {
+  const [name, setname] = useState("");
+  const [image, setimage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return alert("Please Input a username and image");
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      name,
+      id,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    onAddFriend(newFriend);
+    console.log(newFriend);
+    setname("");
+    setimage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>Add a friend</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        placeholder="friend name`"
+        onChange={(e) => setname(e.target.value)}
+      />
       <label>Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        placeholder="friend name`"
+        onChange={(e) => setimage(e.target.value)}
+      />
 
       <button className="button">Add</button>
     </form>
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
+import { useState } from "react";
 import AddFriend from "./AddFriend";
 import "./App.css";
 import BillForm from "./BillForm";
 import FriendList from "./FriendList";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 function App() {
+  const [showAddFriends, setShowAddFriend] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((val) => !val);
+  }
+
+  function handleAddFriends(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <main className="app">
       <div className="sidebar">
-        <FriendList />
-        <AddFriend />
-        <button className="button">select</button>
+        <FriendList friends={friends} />
+        {showAddFriends && <AddFriend onAddFriend={handleAddFriends} />}
+        <button className="button" onClick={handleShowAddFriend}>
+          {!showAddFriends ? "ADD" : "X"}
+        </button>
       </div>
       <BillForm />
     </main>
