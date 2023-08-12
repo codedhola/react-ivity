d17786a6df291104ca07d20179400a649e07249a
codedhola <codedhola2000@gmail.com>
2023-08-12 20:00:27 +0100

    feat: Added fetch with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tempMovieData } from "./assets/data";
 import NavBar from "./NavBar";
 import MainPage from "./MainPage";
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
 
   return (
     <>
       <NavBar movies={movies} />
-      <MainPage movies={movies} />
+      <MainPage movies={movies} setMovies={setMovies} />
     </>
   );
 }
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { tempWatchedData } from "./assets/data";
 import Box from "./Box";
 import MovieList from "./MovieList";
 import WatchLists from "./WatchLists";
 
-const MainPage = ({ movies }) => {
+const api__key = "70d8d442";
+const MainPage = ({ movies, setMovies }) => {
   const [watched, setWatched] = useState(tempWatchedData);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchMovies() {
+      setIsLoading(true);
+      const res = await fetch(
+        `https://www.omdbapi.com/?apikey=${api__key}&s=thor`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+    }
+    fetchMovies();
+  }, []);
 
   return (
     <main className="main">
       <Box>
-        <MovieList movies={movies} />
+        {isLoading ? (
+          <p className="loader"> Loading </p>
+        ) : (
+          <MovieList movies={movies} />
+        )}
       </Box>
 
       <Box>
