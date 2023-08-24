61236fadf9a6d1462b973e5d1858c80bb673c4ba
codedhola <codedhola2000@gmail.com>
2023-08-24 20:38:13 +0100

    refact: cleared usePopcorn branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { useEffect, useState } from "react";
-import { tempMovieData } from "./assets/data";
-import NavBar from "./NavBar";
-import MainPage from "./MainPage";
-import { useMovies } from "./useMovies";
-
 export default function App() {
-  
-  const [query, setQuery] = useState("thor");
-  const [isLoading, movies, error, setMovies ] = useMovies(query)
 
   return (
     <>
-      <NavBar movies={movies} setQuery={setQuery} query={query} />
-      <MainPage movies={movies} setMovies={setMovies} query={query} isLoading={isLoading} error={error} />
+      <h1>Hello</h1>
     </>
   );
 }
