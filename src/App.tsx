2544df7344356bf5ff882f0a72fca184056ca387
codedhola <codedhola2000@gmail.com>
2023-10-03 19:07:55 +0100

    feat: setup reducer for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import Content from "./Content";
 import Header from "./Header";
 
+const initalState = {
+  question: [],
+  status: "loading",
+};
+
+type ACTION =
+  | {
+      type: string;
+      payload: any;
+    }
+  | {
+      type: string;
+      payload: any;
+    };
+
+function reducer(state: typeof initalState, action: ACTION) {
+  switch (action.type) {
+    case "success":
+      return { ...state, question: action.payload, status: "ready" };
+
+    default:
+      throw new Error("An Error Occured in your reducer function");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "success", payload: data }))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <div className="app">
