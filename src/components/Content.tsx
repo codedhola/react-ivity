7d1efc8be3ad751a57b4c207ead241eae635da59
codedhola <codedhola2000@gmail.com>
2023-10-16 19:34:03 +0100

    fix: fix branch bug(git stash issues)

--- src/components/Content.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement, FC } from "react";
-
-type Props = { children: React.ReactNode };
-
-const Content: FC<Props> = ({ children }): ReactElement => {
-  return <main>{children}</main>;
-};
-
-export default Content;
